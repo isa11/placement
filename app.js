7244385d00ad75863dc935a4c0cda456695e747b
simplon <costanzo.isabel@gmail.com>
2017-09-11 09:21:41 +0200

    random suffle fait

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('coucou');
+$.ajax({
+	url : "http://spreadsheets.google.com/feeds/list/1-oYlvGP573O4ml4AzgapLhK_KoEqlfDIzvSnWz48_nQ/od6/public/values?alt=json",
+	
+	success:function(data){
+		
+		//var tab= data.feed.entry;
+
+		function shuffle (data){
+			var chié= data.length, temp, index;
+			while (chié>0){
+				index=Math.floor(Math.random()*chié);
+				chié --;
+				temp=data[chié];
+				data[chié]=data[index];
+				data[index]=temp;
+
+			}
+			return data;
+		}
+		var myChié=data.feed.entry;
+		var tab=shuffle(myChié);
+
+		for(var i = 0 ; i< tab.length;i++){
+			var nom = tab[i].gsx$prénoms.$t;
+			var url= tab[i].gsx$url.$t;
+			$("#img"+i).html(nom + "<br/><img src='" + url +"'/>");
+			
+
+
+
+
+
+		}
+	}
+});
